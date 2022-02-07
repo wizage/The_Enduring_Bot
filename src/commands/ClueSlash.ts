39cd67f1410aef45270a06e7ddd4c5fe8f9ef733
Sam Patzer <wizagesmax@gmail.com>
2022-02-07 08:04:38 -0800

    Updated db connections

---
+++ src/commands/ClueSlash.ts
@@ -0,0 +1,16 @@
+// import { ButtonInteraction, CommandInteraction, EmbedFieldData, GuildMemberRoleManager, Message, MessageActionRow, MessageButton, MessageEmbed, TextChannel, User as DiscordUser } from "discord.js";
+// import { Discord, Slash, SlashOption, SlashGroup, On, ButtonComponent} from "discordx";
+// import { Pagination } from "@discordx/pagination";
+// import { clan } from "runescape-api";
+// import { User } from "../backend/types/User";
+// import { createUser, getUser, verifyUser } from "../backend/models/User.js";
+
+
+// @Discord()
+// export abstract class ClueSlash {
+//   @Slash('clue-title')
+//   private assignClueTitle(){
+
+//   }
+
+// }
