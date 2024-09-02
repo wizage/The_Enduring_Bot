e3661970a83be2a617263bef94f75cd3edc3f065
Sam Patzer <wizagesmax@gmail.com>
2024-09-02 15:18:07 -0700

    card push up

--- src/commands/ModSlash.ts
+++ src/commands/ModSlash.ts
@@ -1,9 +1,11 @@
 import { ChannelType, CommandInteraction, PermissionFlagsBits, PermissionsBitField, Role } from 'discord.js';
 import { Discord, Slash, SlashOption, SlashGroup } from 'discordx';
 import { ApplicationCommandOptionType } from 'discord-api-types/v10';
-import { setupTeam } from '../backend/models/Bingo.js';
-import { BingoCard, BingoItem, BingoRow } from '../types/Bingo';
+import { createCanvas, Image } from 'canvas';
+import { getCard, setupTeam } from '../backend/models/Bingo.js';
+import { BingoCard, BingoItem } from '../types/Bingo';
 import { PVMCard, CluesCard, SkillingCard } from '../constants/cardimport.js';
+
 
 const adminBits = PermissionFlagsBits.KickMembers;
 
@@ -61,10 +63,122 @@ export abstract class ClueSlash {
     const bingoCard: BingoCard = {
       teamid: teamId,
       cardid: cardId,
-      card: ddbCard as unknown as BingoRow[], //Ignore crappy programming
+      card: ddbCard,
     };
     return bingoCard;
   };
+
+  private drawBingoCard = async (cardType: string, cardChecks: BingoCard) : Promise<Buffer> => {
+    
+
+    // Dimensions for the image
+    const size = 5;
+    const squareSize = 150;
+    const width = size * squareSize + 10;
+    const height = width;
+
+    // Instantiate the canvas object
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+    context.font = '150pt Sans';
+
+    // Fill the rectangle with purple
+    context.fillStyle = '#ffffff';
+    context.fillRect(0, 0, width, height);
+
+    // const data = fs.readFileSync(__dirname + `/${cardType}.png`);
+    const imageLoad = new Promise<Image>((resolve) => {
+      var img = new Image;
+      let src = '';
+      if (cardType === 'PVM') {
+        src = 'https://raw.githubusercontent.com/wizage/The_Enduring_Bot/static_images/PVM.png';
+      } else if (cardType === 'Clues') {
+        src = 'https://raw.githubusercontent.com/wizage/The_Enduring_Bot/static_images/Clues.png';
+      } else if (cardType === 'Skilling') {
+        src = 'https://raw.githubusercontent.com/wizage/The_Enduring_Bot/static_images/Skilling.png';
+      }
+      img.src = src;
+      img.onload = () => {
+        resolve(img);
+      };
+    });
+    const img: Image = await imageLoad;
+    context.drawImage(img, 0, 0);
+    cardChecks.card.forEach((row, column) => {
+      row.forEach((slot, index) => {
+        let current = 0;
+        slot.value.forEach((value) => {
+          current += value;
+        });
+        if (current === slot.value.length) {
+          context.fillStyle = '#df5e42';
+          let x = (index * 150 + 20);
+          let y = (column * 150 + 152.5);
+          context.font = '150pt Sans';
+          context.fillText('X', x, y);
+        } else if (current > 0) {
+          context.fillStyle = 'rgba(11, 183, 0, 0.75)';
+          let x = (index * 150 + 5);
+          let y = (column * 150 + 5);
+          // Horizontal bar :
+          // let newY = 150 - ((current / slot.value.length) * 150) + y;
+          // let heightCalc = ((current / slot.value.length) * 150);
+          // context.fillRect(x, newY, 150, heightCalc);
+          // Vertical bar :
+          //let newX = 150 - ((current / slot.value.length) * 150) + x;
+          // let widthCalc = ((current / slot.value.length) * 150);
+          // context.fillRect(x, y, widthCalc, 150);
+          // Vertical small bar :
+          let newY = 125 + y;
+          let widthCalc = ((current / slot.value.length) * 150);
+          context.fillRect(x, newY, widthCalc, 25);
+          context.fillStyle = '#000000';
+          context.font = '14pt Sans';
+          context.fillText(`${current}/${slot.value.length}`, x + 60, newY + 19.5);
+        }
+      });
+    });
+    const buffer = canvas.toBuffer('image/png');
+    return buffer;
+  };
+
+  @Slash({ name: 'draw-card', description: 'Draw new card' })
+  @SlashGroup('mod')
+  async drawCard(
+    @SlashOption({
+      description: 'Provide the team role',
+      name: 'teamrole',
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    teamrole: string,
+    @SlashOption({
+      description: 'Provide the card',
+      name: 'cardtype',
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    cardtype: string,
+    interaction: CommandInteraction,
+  ) {
+    await interaction.deferReply();
+    const roleId = teamrole.replace(/[<@&>]/g, '');
+    console.log(roleId, cardtype);
+    if (cardtype !== 'PVM' && cardtype !== 'Clues' && cardtype !== 'Skilling') {
+      interaction.followUp('Invalid card type');
+      return;
+    }
+
+    const cardChecks = await getCard(cardtype, roleId);
+    if (!cardChecks) {
+      interaction.followUp('role id does not exist');
+      return;
+    }
+
+    const buffer = await this.drawBingoCard(cardtype, cardChecks!);
+
+    await interaction.followUp({ files: [buffer] });
+  }
 
   @Slash({ name:'setup-bingo-team', description: 'Setup a new bingo team' })
   @SlashGroup('mod')
